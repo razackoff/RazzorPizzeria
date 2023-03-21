bbd5687f1d10db5e7afb40cc65c73f3534739ef6
Firuz <razackoff@gmail.com>
2023-03-21 20:25:24 +0500

    Настройка страниц и потока.

---
+++ RazzorPizzeria/Pages/Forms/CustomPizza.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazzorPizzeria.Pages.Forms
+{
+    public class CustomPizzaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ RazzorPizzeria/Pages/Pizza.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazzorPizzeria.Pages
+{
+    public class PizzaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
