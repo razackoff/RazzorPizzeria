7caf0cd69ca31581c2b847baa4988dd2738cf773
Firuz <razackoff@gmail.com>
2023-03-23 21:47:30 +0500

    Cоздание модели и настройка ее в ViewModel.

--- RazzorPizzeria/Pages/Forms/CustomPizza.cshtml.cs
+++ RazzorPizzeria/Pages/Forms/CustomPizza.cshtml.cs
@@ -1,10 +1,13 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazzorPizzeria.Models;
 
 namespace RazzorPizzeria.Pages.Forms
 {
     public class CustomPizzaModel : PageModel
     {
+        [BindProperty]
+        public PizzasModel Pizzas { get; set; }
         public void OnGet()
         {
         }
